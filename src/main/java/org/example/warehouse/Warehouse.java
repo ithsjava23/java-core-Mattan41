528d1470a02e6baf3bb3432ff450d1acedbbe580
Mattan41 <Mats.kruskopf@gmail.com>
2023-10-06 15:19:10 +0200

    removed unnecessary //text

--- src/main/java/org/example/warehouse/Warehouse.java
+++ src/main/java/org/example/warehouse/Warehouse.java
@@ -8,27 +8,6 @@ import java.util.stream.Collectors;
 public class Warehouse {
 
     public static void main(String[] args) {
-/*
-        // todo: ta bort main innan klar
-        // Skapar ett nytt warehouse, ange namn
-        Warehouse warehouse2 = getInstance("Varuhuset");
-        //lägg till products i listan i warehouse
-        UUID uuid1 = UUID.randomUUID();
-        warehouse2.addProduct(uuid1, "Banan",Category.of("fruit"), BigDecimal.valueOf(700,2));
-        warehouse2.addProduct(null, "Ost",Category.of("Dairy"), BigDecimal.valueOf(1300,3));
-        warehouse2.addProduct(null, "Strömming",Category.of("fish"), BigDecimal.valueOf(1300,3));
-
-
-        //System.out.println(warehouse2.addedProducts.stream().toList());
-        System.out.println();
-        //System.out.println(warehouse2.addedProducts.stream().map(Object::toString).collect(Collectors.toList()));
-        List<String> stringList = warehouse2.addedProducts.stream()
-                .map(p -> String.format("%s, %s, %s, %.2f", p.uuid(), p.product(), p.category(), p.price()))
-                .collect(Collectors.toList());
-
-        System.out.println(stringList);
-*/
-
     }
 
     private String name;
@@ -50,8 +29,6 @@ public class Warehouse {
     }
 
     public List<ProductRecord> getProducts() {
-        //return Collections.unmodifiableList(addedProducts);
-        //return List.copyOf(addedProducts);
         return addedProducts.stream().toList();
     }
 
@@ -72,7 +49,6 @@ public class Warehouse {
 
         Optional.ofNullable(price).orElse(price = BigDecimal.ZERO);
 
-        //addedProducts.add(new ProductRecord(uuid, product, category, price));
         addedProducts.add(new ProductRecord(uuid, product, category, price));
         return new ProductRecord(uuid, product, category, price);
 
@@ -84,20 +60,7 @@ public class Warehouse {
     }
 
     public Optional<ProductRecord> getProductById (UUID uuid) {
-
-      /*  for (ProductRecord product : addedProducts) {
-
-                if (ProductRecord.uuid.equals(uuid)) {
-                    return Optional.of(product);
-                }
-        }
-        return Optional.empty();
-
-
-       */
-
         return addedProducts.stream().filter(p -> p.uuid().equals(uuid)).findAny();
-
     }
 
     public void updateProductPrice(UUID uuid, BigDecimal price) {
@@ -119,9 +82,7 @@ public class Warehouse {
         }
     }
         public List<ProductRecord> getChangedProducts() {
-        //todo: ?? Om en productRecord tas bort så ska den tas bort från addedProduct och changedProducts
-                //return List.copyOf(changedProducts);
-                return changedProducts.stream().toList();
+            return changedProducts.stream().toList();
     }
 
     public Map<Category, List<ProductRecord>> getProductsGroupedByCategories() {
@@ -134,7 +95,6 @@ public class Warehouse {
                 .filter(p -> p.category().equals(category))
                 .toList();
     }
-
 
 }
 
